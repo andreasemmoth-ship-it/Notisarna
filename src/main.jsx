19adcfc6b7ed65ec36e1d1b24dbb88080d2047a8
Andreas Emmoth <andreas.emmoth@gmail.com>
2026-06-05 08:14:05 +0200

    feat: add vercel speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App, { ErrorBoundary } from './App.jsx'
 import '../styles.css'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ErrorBoundary>
     <App />
     <Analytics />
+    <SpeedInsights />
   </ErrorBoundary>
 )
